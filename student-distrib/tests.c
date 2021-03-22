bd99f906bebdf914e1944be90663e3aa560ac528
Logan <nylogan2@illinois.edu>
2021-03-22 15:26:01 -0500

    tests added

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -21,7 +21,7 @@ static inline void assertion_failure(){
 /* Checkpoint 1 tests */
 
 /* IDT Test - Example
- * 
+ *
  * Asserts that first 10 IDT entries are not NULL
  * Inputs: None
  * Outputs: PASS/FAIL
@@ -35,7 +35,7 @@ int idt_test(){
 	int i;
 	int result = PASS;
 	for (i = 0; i < 10; ++i){
-		if ((idt[i].offset_15_00 == NULL) && 
+		if ((idt[i].offset_15_00 == NULL) &&
 			(idt[i].offset_31_16 == NULL)){
 			assertion_failure();
 			result = FAIL;
@@ -44,48 +44,75 @@ int idt_test(){
 
 	return result;
 }
-/* divide_error()
-    DESCRIPTION: attempts to divide 2 by 0, handled by interrupt 0x00
-    INPUTS: none
-    OUTPUTS: PASS if successful, interrupt handler if not
-    RETURN VALUE: none
-    SIDE EFFECTS: freezes screen with handler message
-*/
+
 int divide_error() {
 	TEST_HEADER;
 	int num = 2;
 	int denom = 0;
+
 	num /= denom;
+
 	denom = num;
 
 	return PASS;
 }
-/* opcode_error
-    DESCRIPTION: attempts to move from a non-existent register
-    INPUTS: none
-    OUTPUTS: PASS if successful, interrupt handler if not
-    RETURN VALUE: none
-    SIDE EFFECTS: freezes screen with handler message
-*/
-int opcode_error() {
-	TEST_HEADER;
-	asm volatile ("movl %cr6, %eax");
 
+// add more tests here
+
+int keyboard_test() {
+	TEST_HEADER;
 	return PASS;
 }
-/* opcode_error
-    DESCRIPTION: calls the system call handler at index 0x80 of IDT
-    INPUTS: none
-    OUTPUTS: PASS if successful, interrupt handler if not
-    RETURN VALUE: none
-    SIDE EFFECTS: freezes screen with handler message
-*/
-int sys_call_test() {
-	TEST_HEADER;
-	asm volatile ("int $0x80");
 
+/* video_mem_paging_test - CP1
+ * DESCRIPTION: dereferences the video memory portion of virtual memory.
+ * INPUTS: none
+ * OUTPUTS: none
+ * RETURN VALUE: none
+ * SIDE EFFECTS: none
+ */
+int video_mem_paging_test() {
+	TEST_HEADER;
+
+	int* ptr = (int*) 0x000B8000; // test at video memory address
+	int deref;
+	deref = *ptr;
 	return PASS;
 }
+
+/* kernel_paging_test - CP1
+ * DESCRIPTION: dereferences the kernel memory portion of virtual memory.
+ * INPUTS: none
+ * OUTPUTS: none
+ * RETURN VALUE: none
+ * SIDE EFFECTS: none
+ */
+int kernel_paging_test() {
+	TEST_HEADER;
+
+	int* ptr = (int*) 0x00400000; // test at kernel address
+	int deref;
+	deref = *ptr;
+	return PASS;
+}
+
+/* not_present_paging_test - CP1
+ * DESCRIPTION: dereferences the memory portion not present in virtual memory.
+ * INPUTS: none
+ * OUTPUTS: none
+ * RETURN VALUE: none
+ * SIDE EFFECTS: none
+ */
+int not_present_paging_test() {
+	TEST_HEADER;
+
+	int* ptr = (int*) 0x1000000; // test at 16MB - nothing should be there
+	int deref;
+	deref = *ptr; // should cause exception
+	return FAIL;
+}
+
+
 
 /* Checkpoint 2 tests */
 /* Checkpoint 3 tests */
@@ -95,9 +122,10 @@ int sys_call_test() {
 
 /* Test suite entry point */
 void launch_tests(){
-	TEST_OUTPUT("idt_test", idt_test());
+	//TEST_OUTPUT("not_present_paging_test", not_present_paging_test());
+	//TEST_OUTPUT("kernel_paging_test", kernel_paging_test());
+	//TEST_OUTPUT("video_mem_paging_test", video_mem_paging_test());
+
 	// launch your tests here
 	// divide_error();
-	// opcode_error();
-	// sys_call_test();
 }
